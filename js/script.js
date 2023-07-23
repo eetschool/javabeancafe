abd664f9595f7c80f0fd871ded227fa44f9e52d1
eetschool <140295485+eetschool@users.noreply.github.com>
2023-07-22 20:16:39 -0800

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#jqueryelement").hover(function() {
-        $(this).css("color", "gray");
-    },
-    function() {
-        $(this).css("color", "blue");
-    });
-});
